b0013686693a57a061a99f85bf55f8ec61c59f39
Bijay Gurung <4636315+bglearning@users.noreply.github.com>
2023-06-30 14:34:26 +0200

    Change frame-read timeout to be 100x higher

--- video2numpy/read_vids_cv2.py
+++ video2numpy/read_vids_cv2.py
@@ -44,7 +44,7 @@ def read_vids(vid_refs, worker_id, take_every_nth, resize_size, batch_size, queu
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         res = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         minutes = (frame_count / fps) / 60
-        timeout = max(minutes, 0.5)  # acceptable reading speed is 1 [min downloaded/s]
+        timeout = max(minutes, 50)  # acceptable reading speed is 1 [min downloaded/s]
         timeout *= res / 360.0  # give more time for longer vids
 
         if not cap.isOpened():
